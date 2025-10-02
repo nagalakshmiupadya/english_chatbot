e21d6eac91dc75e8d4fd6496ceda3b22c01e0c56
Nagalakshmi Upadya <123979970+nagalakshmiupadya@users.noreply.github.com>
2025-10-02 14:15:02 +0530

    Update english.py

--- english.py
+++ english.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask,render_template,request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
 
@@ -169,4 +169,5 @@ Question: """ + user_input
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
